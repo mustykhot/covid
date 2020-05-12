65702b73004e2479d456b57b19a61bce7e25ecd6
mustykhot <45202256+mustykhot@users.noreply.github.com>
2020-05-12 12:20:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleDropdown() {
+    let navbarToggle = document.getElementById("navbar-toggle");
+    if (navbarToggle.className === 'navbar') {
+        navbarToggle.className += ' responsive';
+
+    } else {
+        navbarToggle.className = 'navbar';
+    }
+
+}
